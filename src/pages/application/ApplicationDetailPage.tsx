a08ff5cca6bc6e4bfc67ee26cfcc1df7ebcef859
fillip1984 <fillip1984@gmail.com>
2023-04-09 02:18:18 -0400

    added ability to delete applications and added in more button customizations but probably a mistake...

--- src/pages/application/ApplicationDetailPage.tsx
+++ src/pages/application/ApplicationDetailPage.tsx
@@ -1,14 +1,18 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import {
   applicationKeys,
+  deleteApplicationById,
   readApplicationById,
 } from "../../services/ApplicationServices";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import LoadingScreen from "../../components/navigation/LoadingScreen";
 
 const ApplicationDetail = () => {
   const { id } = useParams();
   const applicationId = Number(id);
+
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const {
     data: application,
@@ -20,6 +24,19 @@ const ApplicationDetail = () => {
     queryFn: () => readApplicationById(applicationId),
     refetchOnWindowFocus: false,
   });
+
+  const { mutate: deleteApplicationByIdMutator } = useMutation(
+    deleteApplicationById,
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(applicationKeys.all);
+        navigate("/applications");
+      },
+      onError: () => {
+        alert("Error");
+      },
+    }
+  );
 
   return (
     <div className="container">
@@ -33,8 +50,17 @@ const ApplicationDetail = () => {
 
       {!isLoading && !isError && (
         <>
+          <div className="my-2 flex gap-2">
+            <button
+              type="button"
+              className="secondary-btn secondary-btn-danger"
+              onClick={() => deleteApplicationByIdMutator(application.id)}>
+              Delete
+            </button>
+          </div>
           <h2>{application?.name}</h2>
-          {application?.description}
+
+          <p className="mt-4">{application?.description}</p>
         </>
       )}
     </div>
