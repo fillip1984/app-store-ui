b99edab25057c9b5ed6699427d074da6d94fad36
fillip1984 <fillip1984@gmail.com>
2023-04-08 23:48:30 -0400

    still working out zod

--- src/Types.ts
+++ src/Types.ts
@@ -4,15 +4,15 @@ const BaseSchema = z.object({
   id: z.number(),
 });
 
-export const ApplicationSummarySchema = z.object({
-  name: z.string().min(2).max(100),
-  description: z.string().min(10).max(500),
-  //waiting on: https://github.com/colinhacks/zod/issues/310
-  repositoryUrl: z.string().url().optional().or(z.literal("")),
-});
+export const ApplicationSummarySchema = z
+  .object({
+    name: z.string().min(2).max(100),
+    description: z.string().min(10).max(500),
+    //waiting on: https://github.com/colinhacks/zod/issues/310
+    repositoryUrl: z.string().url().optional().or(z.literal("")),
+  })
+  .merge(BaseSchema);
+export type ApplicationSummary = z.infer<typeof ApplicationSummarySchema>;
 
-const ApplicationSummary = ApplicationSummarySchema.merge(BaseSchema);
-export type ApplicationSummary = z.infer<typeof ApplicationSummary>;
-
-const ApplicationSchema = ApplicationSummary;
+const ApplicationSchema = ApplicationSummarySchema;
 export type Application = z.infer<typeof ApplicationSchema>;
